9e0e9de02bae2eb36c0e24cc009fea4cfd703ba5
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-09-14 09:18:34 +1200

    Separate the blog data from the js, to a JSON file

--- js/blog.js
+++ js/blog.js
@@ -1,47 +1,9 @@
-const data = [
-  {
-    heading: 'Technical',
-    posts: [
-      {
-        id: 0,
-        url: '/blog/te-houtaewa-template.html',
-        title: 'The Story of Te Houtaewa (template)',
-      },
-      {
-        id: 1,
-        url: '/blog/html-css.html',
-        title: 'CSS Concepts - Margin, Border and Padding',
-      },
-      {
-        id: 2,
-        url: '/blog/javascript-dom.html',
-        title: 'JavaScript and the DOM',
-      },
-    ],
-  },
-  {
-    heading: 'Core',
-    posts: [
-      {
-        id: 0,
-        url: '/blog/identity-values.html',
-        title: 'Exploration of Identity, Values, and Strengths',
-      },
-      {
-        id: 1,
-        url: '/blog/learning-plan.html',
-        title: 'Dev Academy Learning Plan',
-      },
-      {
-        id: 2,
-        url: '/blog/emotional-intelligence.html',
-        title: 'Emotional Intelligence Exploration',
-      },
-    ],
-  },
-]
+blog('blog', './data.json')
 
-function blog(blogElementId, blogData) {
+async function blog(blogElementId, blogDataJSON) {
+  const response = await fetch(blogDataJSON)
+  const blogData = await response.json()
+
   // find the blog div + create a list from each type of blog
   const blogElement = document.getElementById(blogElementId)
   blogData.forEach((sectionData) => blogSection(blogElement, sectionData))
@@ -75,5 +37,3 @@ function blogListItem(sectionList, post) {
   link.setAttribute('href', 'post.url')
   link.innerHTML = post.title
 }
-
-blog('blog', data)
